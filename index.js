eadad7f94e4c00ebe467b742a5845a7c9769369d
Arshpreet-Kaur-23 <140833553+Arshpreet-Kaur-23@users.noreply.github.com>
2024-08-26 20:10:09 +0530

    setting up routes and controllers

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const app = express();
 const PORT = 5000;
-app.get("/",(req,res)=>{
-    res.send("Hi,I am live");
+
+const products_routes = require("./routes/products");
+
+app.get("/", (req, res) => {
+    res.send("Hi, I am live");
 });
-const start = async() =>{
-    try{
-        app.listen(PORT,()=>{
-            `${PORT} Yes I am connected`
-        })
-    }catch(error){
+
+app.use("/api/products", products_routes);
+
+const start = async () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`${PORT} Yes, I am connected`);
+        });
+    } catch (error) {
         console.log(error);
     }
 }
+
 start();
